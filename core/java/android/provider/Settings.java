1bcb803ba50a55b73d2515f5b9a78b217607010e
maxwen <max.weninger@gmail.com>
2020-11-11 16:04:30 +0100

    Allow changing back gesture height [1/2]
    * @neobuddy89: Simplify and cleanup!
    
    Change-Id: I5aa37f208150f7f9f7080ea2fb00d04607857d1c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6630,6 +6630,9 @@ public final class Settings {
         @Readable
         public static final String NAVBAR_LONG_PRESS_GESTURE = "navbar_long_press_gesture";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
