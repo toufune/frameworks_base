365edd3abdb456c3bb8dfa1680c471bd4ea44314
Pranav Vashi <neobuddy89@gmail.com>
2021-12-11 16:12:08 +0530

    SystemUI: Properly apply dark theming to clock
    * This is forward port from Android 11.
    
    Change-Id: Ib24d7e000be19063de43df98e96a330487d28ce7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarViewController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarViewController.kt
@@ -256,12 +256,10 @@ private constructor(
 
     private fun addDarkReceivers() {
         darkIconDispatcher.addDarkReceiver(battery)
-        clockController.addDarkReceiver()
     }
 
     private fun removeDarkReceivers() {
         darkIconDispatcher.removeDarkReceiver(battery)
-        clockController.removeDarkReceiver()
     }
 
     inner class PhoneStatusBarViewTouchHandler : Gefingerpoken {
