1eeeca77acb6263b3a0a867acc377aebadb20e25
nift4 <nift4@protonmail.com>
2023-05-22 16:03:14 +0200

    wm: Fix menu in legacy apps with multiple activities
    To reproduce:
    1. Open pre-honeycomb app in activity without options menu
    2. adb shell input keyevent MENU
    3. Observe menu not opening (correct behaviour)
    4. Open activity with options menu in same app (without restarting app)
    4. adb shell input keyevent MENU
    5. Observe nothing happening (wrong behaviour)
    6. adb shell input keyevent MENU
    7. Observe menu opening and closing itself instantly (wrong behaviour)
    
    Root cause:
    When skipping creating empty panel, we do not update state to closed, so
    app and window will disagree about menu open or not, which will cause
    conflict between open and closed state.
    
    Fixes: 3c2bfc88 ("CP: Don't create panel if content is empty")
    Test: Reproduce steps now successfully open menu every time in activity
          with options menu.
    Change-Id: I0501a2ffcccd30b0085dea7e2105ad57869d00da

--- core/java/com/android/internal/policy/PhoneWindow.java
+++ core/java/com/android/internal/policy/PhoneWindow.java
@@ -984,6 +984,7 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
 
             // This will populate st.shownPanelView
             if (!initializePanelContent(st) || !st.hasPanelItems()) {
+                closePanel(st, true);
                 return;
             }
 
@@ -1032,6 +1033,7 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
         if (!st.hasPanelItems()) {
             // Ensure that |st.decorView| has its actual content. Otherwise, an empty window can be
             // created and cause ANR.
+            closePanel(st, true);
             return;
         }
 
