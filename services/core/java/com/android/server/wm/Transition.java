a7e9e3b416c74c2f744b3d93d63808862955dc01
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-12 08:40:17 +0800

    services: Implement transition boosting
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I3e650fc7cfa2a7090aa2de93793baa821801f592
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/Transition.java
+++ services/core/java/com/android/server/wm/Transition.java
@@ -95,12 +95,14 @@ import android.content.pm.ActivityInfo;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.HardwareBuffer;
+import android.hardware.power.Boost;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Debug;
 import android.os.IBinder;
 import android.os.IRemoteCallback;
 import android.os.Looper;
+import android.os.PowerManagerInternal;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.Trace;
@@ -126,6 +128,7 @@ import com.android.internal.policy.TransitionAnimation;
 import com.android.internal.protolog.ProtoLog;
 import com.android.internal.protolog.WmProtoLogGroups;
 import com.android.internal.util.function.pooled.PooledLambda;
+import com.android.server.LocalServices;
 import com.android.server.inputmethod.InputMethodManagerInternal;
 import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.window.flags.Flags;
@@ -198,6 +201,8 @@ class Transition implements BLASTSyncEngine.TransactionReadyListener {
     final WindowManagerService mWmService;
     private final BLASTSyncEngine mSyncEngine;
     private final Token mToken;
+
+    private final PowerManagerInternal mPowerManagerInternal;
 
     private @Nullable ActivityRecord mPipActivity;
 
@@ -382,6 +387,8 @@ class Transition implements BLASTSyncEngine.TransactionReadyListener {
 
         mLogger.mCreateWallTimeMs = System.currentTimeMillis();
         mLogger.mCreateTimeNs = SystemClock.elapsedRealtimeNanos();
+
+        mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
     }
 
     @Nullable
@@ -771,6 +778,10 @@ class Transition implements BLASTSyncEngine.TransactionReadyListener {
             return;
         }
         mState = STATE_STARTED;
+        if (mPowerManagerInternal != null && mType == TRANSIT_CHANGE) {
+            mPowerManagerInternal.setPowerBoost(Boost.INTERACTION, 80);
+            mPowerManagerInternal.setPowerBoost(Boost.DISPLAY_UPDATE_IMMINENT, 80);
+        }
         ProtoLog.v(WmProtoLogGroups.WM_DEBUG_WINDOW_TRANSITIONS, "Starting Transition %d",
                 mSyncId);
         applyReady();
