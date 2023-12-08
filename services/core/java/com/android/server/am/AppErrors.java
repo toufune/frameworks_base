f521e7560fed4a217e95257fde9cbb6026baf11d
Jiangyou Li <jiangyou.li@unisoc.com>
2023-12-08 15:28:05 +0800

    Don't show app error dialog before system booted
    If here show app error dialog when system is booting,
    it will call ensureBootCompleted to set mBooted.
    And then disturbs booting process.
    Finally, finishBooting can not be executed.
    
    Change-Id: Ie94b9e7b204e8d1948a1c1b846967ff2b0535e24

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -674,6 +674,10 @@ class AppErrors {
             if (r == null || !makeAppCrashingLocked(r, shortMsg, longMsg, stackTrace, data)) {
                 return;
             }
+            // don't show app error dialog before system booted
+            if (!mService.mBooting && !mService.mBooted) {
+                return;
+            }
 
             // Add paste content for Memochō option
             String devFP = SystemProperties.get("ro.vendor.build.fingerprint", "");
