68aa70155c76371ba978acbb75d5d4075e001841
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-30 17:43:55 +0800

    SystemUI: blur rendering enhancements
    Change-Id: I024055b5ce6e0dc080e74b4d8b5598c05a2a56ba

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayoutController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayoutController.java
@@ -1480,7 +1480,7 @@ public class NotificationStackScrollLayoutController implements Dumpable {
             mView.setRenderEffect(RenderEffect.createBlurEffect(
                     blurRadius,
                     blurRadius,
-                    Shader.TileMode.CLAMP));
+                    Shader.TileMode.MIRROR));
         } else {
             debugLog("Resetting blur RenderEffect for NotificationStackScrollLayoutController");
             mView.setRenderEffect(null);
