0d29512e7c6d8a4da479e5130beb4ac04dbc5a33
Pranav Vashi <neobuddy89@gmail.com>
2025-08-18 18:14:10 +0530

    SystemUI: Fix up OmniJaws weather view on lockscreen
    Change-Id: Ie4a22d4ca0c921fe2d427dbf5b191cd09ca9a730
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/lockscreen/LockscreenSmartspaceController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/lockscreen/LockscreenSmartspaceController.kt
@@ -312,7 +312,7 @@ constructor(
             return showWeather
         }
 
-    val isCustomWeatherEnabled: Boolean
+    val isOmniWeatherEnabled: Boolean
         get() {
             val showCustomWeather =
                 systemSettings.getIntForUser(
@@ -323,7 +323,8 @@ constructor(
             return showCustomWeather && !isWeatherEnabled
         }
 
-    val isEnabled: Boolean = plugin != null && isWeatherEnabled
+    val isEnabled: Boolean
+        get() = plugin != null && isWeatherEnabled
 
     private fun updateBypassEnabled() {
         val bypassEnabled = bypassController.bypassEnabled
