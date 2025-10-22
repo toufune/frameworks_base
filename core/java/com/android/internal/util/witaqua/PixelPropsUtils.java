ec1987e856f2753ccb44a4c3af4033b495a20fe7
Pranav Vashi <neobuddy89@gmail.com>
2025-10-22 11:42:50 +0530

    PixelPropsUtils: Add more logs for debugging
    Change-Id: I8c1ff65685b9f831c887a27f5f9c8148d3eb119c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/witaqua/PixelPropsUtils.java
+++ core/java/com/android/internal/util/witaqua/PixelPropsUtils.java
@@ -239,6 +239,7 @@ public final class PixelPropsUtils {
     public static void setProps(Context context) {
         final String packageName = context.getPackageName();
         if (packageName == null || packageName.isEmpty()) {
+            if (DEBUG) Log.d(TAG, "Null received in setProps.");
             return;
         }
 
@@ -250,12 +251,13 @@ public final class PixelPropsUtils {
             if (packageName.equals("com.google.android.apps.photos")) {
                 if (Settings.Secure.getInt(context.getContentResolver(),
                         Settings.Secure.PI_PHOTOS_SPOOF, 1) == 1) {
+                    if (DEBUG) Log.d(TAG, "Gphotos spoofing disabled by setting");
                     propsToChange = propsToChangePixelXL;
                 }
             } else if (packageName.equals("com.netflix.mediaclient") &&
                         Settings.Secure.getInt(context.getContentResolver(),
                         Settings.Secure.PI_NETFLIX_SPOOF, 0) != 1) {
-                    if (DEBUG) Log.d(TAG, "Netflix spoofing disabled by system prop");
+                    if (DEBUG) Log.d(TAG, "Netflix spoofing disabled by setting");
                     return;
             } else if (packageName.equals("com.google.android.gms")) {
                 final String processName = Application.getProcessName().toLowerCase();
@@ -335,6 +337,7 @@ public final class PixelPropsUtils {
 
     private static boolean isDeviceTablet(Context context) {
         if (context == null) {
+            if (DEBUG) Log.d(TAG, "Null received in isDeviceTablet.");
             return false;
         }
         Configuration config = context.getResources().getConfiguration();
@@ -373,6 +376,7 @@ public final class PixelPropsUtils {
     private static void spoofBuildGms(Context context) {
         if (Settings.Secure.getInt(context.getContentResolver(),
                 Settings.Secure.PI_ENABLE_SPOOF, 1) != 1) {
+            if (DEBUG) Log.d(TAG, "GMS spoofing disabled by setting");
             return;
         }
 
@@ -380,6 +384,7 @@ public final class PixelPropsUtils {
         long mtime = dataFile.exists() ? dataFile.lastModified() : -1;
 
         if (mtime == sCertPropsMtime && sCertifiedProps != null && !sCertifiedProps.isEmpty()) {
+            if (DEBUG) Log.d(TAG, "New certification props not found, applying existing ones");
             applyCertifiedProps();
             return;
         }
@@ -387,6 +392,7 @@ public final class PixelPropsUtils {
         String savedProps = readFromFile(dataFile);
         List<String> fresh = new ArrayList<>();
         if (TextUtils.isEmpty(savedProps)) {
+            if (DEBUG) Log.d(TAG, "Certification props not available! Not applied.");
             return;
         }
         if (DEBUG) Log.d(TAG, "Parsing props fetched by attestation service");
@@ -405,6 +411,7 @@ public final class PixelPropsUtils {
         sCertifiedProps = new ArrayList<>(fresh);
         sCertPropsMtime = mtime;
         if (sCertifiedProps != null && !sCertifiedProps.isEmpty()) {
+            if (DEBUG) Log.d(TAG, "New certification props found, applying new ones");
             applyCertifiedProps();
         }
     }
@@ -449,11 +456,16 @@ public final class PixelPropsUtils {
         Context context = ActivityThread.currentApplication() != null
                 ? ActivityThread.currentApplication().getApplicationContext()
                 : null;
-        if (context == null) return;
+        if (context == null) {
+            if (DEBUG) Log.d(TAG, "Null received in onEngineGetCertificateChain.");
+            return;
+        }
 
         if (Settings.Secure.getInt(context.getContentResolver(),
-                Settings.Secure.PI_ENABLE_SPOOF, 1) != 1)
+                Settings.Secure.PI_ENABLE_SPOOF, 1) != 1) {
+            if (DEBUG) Log.d(TAG, "onEngineGetCertificateChain disabled by setting");
             return;
+        }
         // If a keybox is found, don't block key attestation
         if (Settings.Secure.getInt(context.getContentResolver(),
                 Settings.Secure.PI_GMS_CERT_CHAIN, 0) == 1
