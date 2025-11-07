9c47191af40d0a3deecbf3fd1c31b33c499af8f2
cjybyjk <cjybyjk@zjnu.edu.cn>
2025-11-07 23:24:45 +0100

    base: Show Chinese lunar calendar in QS [1/2]
    [nurkeinneid] updated to android 16
    
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Change-Id: Ie8187746f17c9a6773305e8c24e642c84dc81685
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -129,7 +129,8 @@ public class SystemSettings {
                 Settings.System.PREFERRED_REGION,
                 Settings.System.CV_ENABLED,
                 Settings.System.CV_DYNAMIC_ENABLED,
-                Settings.System.CV_PREFERRED_INTENSITY
+                Settings.System.CV_PREFERRED_INTENSITY,
+                Settings.System.QS_SHOW_LUNAR_CALENDAR
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
